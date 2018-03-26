eb538083e00424d48b0de16e53eec68745e78b9c
f3igao <f3igao@icloud.com>
2018-03-26 12:12:21 -0400

    change seed file and fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,12 @@ end
 
 Story.delete_all
 Story.reset_pk_sequence
-User.all.each do |user|
-  content = Faker::HarryPotter.quote
-  location = Faker::Address.latitude + ',' + Faker::Address.longitude
-  story = Story.create(content:content, location:location, user_id:user.id)
+2.times do
+  User.all.each do |user|
+    content = Faker::HarryPotter.quote
+    location = Faker::Address.latitude + ',' + Faker::Address.longitude
+    Story.create(content:content, location:location, user_id:user.id)
+  end
 end
 
 
